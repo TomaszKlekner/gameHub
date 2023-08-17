ed9d970966052c58411f8ccc3c7c5b1728e9701b
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-08-17 11:32:34 +0200

    Pass children to a react component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import Alert from './components/Alert';
+
 function App() {
+  let items = ['New York', 'Tokyo', 'Warsaw', 'Paris', 'London'];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
-    <>
-      <h1>Home Page</h1>
-    </>
+    <div className='container my-5'>
+      <Alert>
+        <span>Hello World!</span>
+      </Alert>
+    </div>
   );
 }
 
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+interface Props {
+  items: string[];
+  heading: string;
+  onSelecItem: (item: string) => void;
+}
+
+const ListGroup = ({ items, heading, onSelecItem }: Props) => {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <div className='card' style={{ width: '18rem' }}>
+      <div className='card-header'>
+        <h1>{heading}</h1>
+      </div>
+      <ul className='list-group'>
+        {items.map((item, index) => (
+          <li
+            key={index}
+            className={
+              'list-group-item' + (selectedIndex === index ? ' active' : '')
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelecItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ListGroup;
