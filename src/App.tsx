724fee30f94ff1c162b4e6d77e0876495b7e0a52
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-08-17 11:35:36 +0200

    Update content

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
 import Alert from './components/Alert';
 
 function App() {
-  let items = ['New York', 'Tokyo', 'Warsaw', 'Paris', 'London'];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
-  return (
-    <div className='container my-5'>
-      <Alert>
-        <span>Hello World!</span>
-      </Alert>
-    </div>
-  );
+  return <div className='container my-5'></div>;
 }
 
 export default App;
