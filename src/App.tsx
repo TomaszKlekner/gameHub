78df24e26a0d01b07a924d4fb3ed2b69f49a8a1a
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-08-18 08:19:38 +0200

    Update arrays

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import Message from './components/Message';
+import { useState } from 'react';
 
 function App() {
+  const [drink, setDrink] = useState({
+    title: 'Americano',
+    price: 5,
+  });
+
+  // Arrays should be treated as immutable or read-only
+  // Use the spread operator to copy all of the fields
+  const handleClick = () => {
+    setDrink({ ...drink, price: 6 });
+  };
+
   return (
     <div className='container my-5'>
-      <Message />
-      <Message />
-      <Message />
+      <button onClick={handleClick}>Click Me</button>
+      <p>Drink Price: {drink.price}</p>
     </div>
   );
 }
