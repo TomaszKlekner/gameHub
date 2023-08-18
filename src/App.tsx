23591cc98dd5aeeb274bd894746006b61141deb4
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-08-18 08:27:23 +0200

    Adding,  removing and updating array content

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,24 @@
 import { useState } from 'react';
 
 function App() {
-  const [customer, setCustomer] = useState({
-    name: 'John',
-    address: {
-      city: 'San Francisco',
-      zipCode: 94111,
-    },
-  });
+  const [tags, setTags] = useState(['happy', 'cheerful']);
 
   // Nested Objects should be treated as immutable or read-only
   // Use the spread operator to copy all of the fields
   // The spread operator creates a shallow copy
   const handleClick = () => {
-    setCustomer({
-      ...customer,
-      address: { ...customer.address, zipCode: 94112 },
-    });
+    // Add a new item to the array
+    setTags([...tags, 'exciting']);
+
+    // Remove
+    setTags(tags.filter((tag) => tag !== 'happy'));
+
+    // Update
+    setTags(tags.map((tag) => (tag === 'happy' ? 'happyness' : tag)));
   };
 
   return (
     <div className='container my-5'>
-      <h1>Customer</h1>
-      <ul>
-        <li>{customer.name}</li>
-        <li>{customer.address.city}</li>
-        <li>{customer.address.zipCode}</li>
-      </ul>
-
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
