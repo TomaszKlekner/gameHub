1c295b85b29508c3bd49ef2195a16c768cd3d3ec
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-08-18 09:53:23 +0200

    Create a reusable component as practise

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,9 @@
-import { useState } from 'react';
+import ExpandableText from './components/ExpandableText/ExpandableText';
 
 function App() {
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      {
-        id: 1,
-        title: 'Product 1',
-        quantity: 1,
-      },
-      {
-        id: 2,
-        title: 'Product 2',
-        quantity: 1,
-      },
-    ],
-  });
-
-  const handleClick = () => {
-    // Update product 1 quantity to 2
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
-      ),
-    });
-  };
-
   return (
     <div className='container my-5'>
-      <h2>Shopping Cart</h2>
-      <p>Discount: {cart.discount}</p>
-      <ul>
-        {cart.items.map((item) => (
-          <li key={item.id}>
-            {item.id}: {item.title} - Q: {item.quantity}
-          </li>
-        ))}
-      </ul>
-      <button onClick={handleClick}>Update Product</button>
+      <ExpandableText>Hello World</ExpandableText>
     </div>
   );
 }
