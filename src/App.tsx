0e98d8ed6c9a75791132569990b282eb73fb66a2
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-08-18 08:24:09 +0200

    Updating nested objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
 import { useState } from 'react';
 
 function App() {
-  const [drink, setDrink] = useState({
-    title: 'Americano',
-    price: 5,
+  const [customer, setCustomer] = useState({
+    name: 'John',
+    address: {
+      city: 'San Francisco',
+      zipCode: 94111,
+    },
   });
 
-  // Arrays should be treated as immutable or read-only
+  // Nested Objects should be treated as immutable or read-only
   // Use the spread operator to copy all of the fields
+  // The spread operator creates a shallow copy
   const handleClick = () => {
-    setDrink({ ...drink, price: 6 });
+    setCustomer({
+      ...customer,
+      address: { ...customer.address, zipCode: 94112 },
+    });
   };
 
   return (
     <div className='container my-5'>
+      <h1>Customer</h1>
+      <ul>
+        <li>{customer.name}</li>
+        <li>{customer.address.city}</li>
+        <li>{customer.address.zipCode}</li>
+      </ul>
+
       <button onClick={handleClick}>Click Me</button>
-      <p>Drink Price: {drink.price}</p>
     </div>
   );
 }
