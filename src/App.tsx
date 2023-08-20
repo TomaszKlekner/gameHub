cd50b3de08767cf9e5a77338478427994cc28d57
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-08-20 12:38:47 +0200

    useEffect hook introduction

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import ExpenseTracker from './components/ExpenseTracker/ExpenseTracker';
+import { useEffect, useRef } from 'react';
 
 function App() {
+  const ref = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (ref.current) ref.current.focus();
+  }, []);
+
   return (
     <div className='container my-5'>
-      <ExpenseTracker />
+      <label htmlFor='' className='form-label'>
+        Example text
+      </label>
+      <input type='text' className='form-control' ref={ref} />
     </div>
   );
 }
