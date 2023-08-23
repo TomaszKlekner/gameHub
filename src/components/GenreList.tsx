a207933f601e802421b5f50d94bac95cdbb2bc9f
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-08-23 14:17:00 +0200

    Fetch the genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,20 @@
+import { Text } from '@chakra-ui/react';
+
+import useGenres from '../hooks/useGenres';
+
+const GenreList = () => {
+  const { genres, error } = useGenres();
+  return (
+    <div>
+      GenreList
+      {error && <Text>{error}</Text>}
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre.id}>{genre.slug}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default GenreList;
