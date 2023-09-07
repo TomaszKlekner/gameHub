ad4f05949d490bc9e5a91f671b90779b07818d46
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-09-07 15:53:31 +0200

    Remove api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '179044740d0042b080b43263ad1d0600',
+    key: import.meta.env.RAWG_API_KEY,
   },
 });
