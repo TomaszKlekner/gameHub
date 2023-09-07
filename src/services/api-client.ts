d9b50b0e64e4a2107d216f2a0ecaad19610a3ed6
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-09-07 15:56:30 +0200

    Update api key url

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: import.meta.env.RAWG_API_KEY,
+    key: process.env.RAWG_API_KEY,
   },
 });
