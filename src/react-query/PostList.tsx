fe7ecaf042ff2e14a006e956b7a06c320885f47f
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-12-28 13:05:26 +0100

    Use Infinite query

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react';
+import React from 'react';
 import { usePosts } from '../hooks/usePosts';
 
 export const PostList = () => {
   const pageSize = 10;
-  const [page, setpage] = useState(1);
   const {
     data: postList,
     isLoading,
     isError,
+    isFetchingNextPage,
     error,
+    fetchNextPage,
   } = usePosts({
-    page,
     pageSize,
   });
 
@@ -21,40 +21,24 @@ export const PostList = () => {
   return (
     <>
       <ul className='list-group'>
-        {postList?.map((post) => (
-          <li className='list-group-item' key={post.id}>
-            User {post.userId} : Post {post.id} - {post.title}
-          </li>
+        {postList.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.map((post) => (
+              <li className='list-group-item' key={post.id}>
+                User {post.userId} : Post {post.id} - {post.title}
+              </li>
+            ))}
+          </React.Fragment>
         ))}
       </ul>
 
-      <nav className='mt-4' aria-label='Page Pagination'>
-        <ul className='pagination pagination-md'>
-          <li className='page-item'>
-            <button
-              onClick={() => setpage(page - 1)}
-              className={page === 1 ? 'page-link disabled' : 'page-link'}
-              aria-label='Previous'
-            >
-              <span aria-hidden='true'>&laquo;</span>
-            </button>
-          </li>
-
-          <li className='page-item'>
-            <a className='page-link'>{page}</a>
-          </li>
-
-          <li className='page-item'>
-            <button
-              onClick={() => setpage(page + 1)}
-              className={page === pageSize ? 'page-link disabled' : 'page-link'}
-              aria-label='Next'
-            >
-              <span aria-hidden='true'>&raquo;</span>
-            </button>
-          </li>
-        </ul>
-      </nav>
+      <button
+        disabled={isFetchingNextPage}
+        onClick={() => fetchNextPage()}
+        className='btn btn-primary mt-4'
+      >
+        {isFetchingNextPage ? 'Loading' : 'Load More'}
+      </button>
     </>
   );
 };
